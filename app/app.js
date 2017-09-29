ccce310269d38f4f6620c59857d6494ae10844af
Sorabh <ssorabh.ssharma@gmail.com>
2017-09-29 20:06:50 +0530

    basic application with paypal sandbox complete

--- app/app.js
+++ app/app.js
@@ -3,8 +3,6 @@
 // Declare app level module which depends on views, and components
 angular.module('webStore', [
   'ngRoute',
-  'webStore.view1',
-  'webStore.view2',
   'webStore.templates'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
